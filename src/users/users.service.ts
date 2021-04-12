0a0e41f22b6ae717ecbaa4df36f291661813c435
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2021-04-12 11:51:11 +0200

    create feature module for user domain

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,10 @@ import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
-
   register(): User {
     return {
       id: '1',
-      name: 'zero!',
+      name: 'zero-based',
       secret: 'WKSFANU2PIIOF5GK',
       email: 'zerobasedteam@gmail.com',
     };
