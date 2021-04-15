83d84a0890474dc515d73f19ea334ce9d1ee4c95
monicatanios <monicatanios@gmail.com>
2021-04-15 14:55:27 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Tazkrtak')
+    .setDescription('Tazkrtak API description')
+    .setVersion('1.0')
+    .addTag('tazkrtak')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
