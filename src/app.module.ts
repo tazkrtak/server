32a5114c58719ee813f58752b36339218c9df43e
monicatanios <monicatanios@gmail.com>
2021-04-18 16:39:45 +0200

    fix: remove hello world endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { ScannersModule } from './scanner/scanners.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ScannersModule, TicketsModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
